2d6f010f24580f4889d6a026534c02dfd47ba75c
marko <marko.kavcic92@gmail.com>
2021-12-09 20:53:39 +0100

    changes to day9 pt1

--- day9_pt1.py
+++ day9_pt1.py
@@ -2,21 +2,19 @@
 data = open('inputs/day9.txt').read().splitlines()
 
 points = []
+rows = len(data)
+cols = len(data[0])
 
 def calculate_pts(row,col,rows,cols):
-    pts = min(data[row + ri][col + ci] 
-             for ri, ci in [(-1, 0), (1, 0), (0, -1), (0, 1)] 
-             if 0 <= row + ri < rows 
-             and 0 <= col + ci < cols)
-    return pts
+    pts = min(data[row + rind][col + cind] ##increment through data
+             for rind, cind in [(-1, 0), (1, 0), (0, -1), (0, 1)] #index directions
+             if 0 <= row + rind < rows 
+             and 0 <= col + cind < cols)
+    return pts 
 
+for row in range(rows):
+    for col in range(cols):
+        if data[row][col] < calculate_pts(row,col,rows,cols): #recursive to sum pts
+            points.append(int(data[row][col]) + 1)
 
-def calculate_risk(rows, cols):
-    rows, cols = len(data), len(data[0])
-    for row in range(rows):
-        for col in range(cols):
-            if data[row][col] < calculate_pts(row,col,rows,cols): #recursive to later sum
-                points.append(int(data[row][col]) + 1)
-
-calculate_risk(len(data) ,data[0])
 print(sum(points))
